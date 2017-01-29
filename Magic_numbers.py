f0fdc8883cada8012d6d6a18d0d102881d2a9e43
LeoGSA <leogsa@gmail.com>
2017-01-29 20:43:29 +0300

    minor changes

--- Magic_numbers.py
+++ Magic_numbers.py
@@ -9,7 +9,8 @@ full_matrix = [
     ['/video', r'464c56', '/video/', '.flv'],                           # '464c56' = FLV
     ['/video', r'52494646........415649', '/video/', '.avi'],           # '52494646........415649' = RIFF    AVI
     ['/music', r'494433', '/music/', '.mp3'],                           # '494433' = ID3
-    ['/html', r'3c21444f43545950452068746d6c3e', '/html/', '.html'],    # !DOCTYPE html
+    ['/html', r'3c21646f63747970652068746d6c', '/html/', '.html'],      # !doctype html
+    ['/html', r'3c21444f43545950452068746d6c', '/html/', '.html'],      # !DOCTYPE html
     ['/doc', r'd0cf11e0a1b11ae1', '/doc/', '.doc'],                     # DOC or XLS or PPT
     ['/pdf', r'25504446', '/pdf/', '.pdf'],                             # '25504446' = %PDF
     ['/zip', r'504b0304', '/zip/', '.zip'],                             # ZIP or DOCX or ...
